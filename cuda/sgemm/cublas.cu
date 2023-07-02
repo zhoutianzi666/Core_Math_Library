a167aa24d8e817faa24384f29e6baef43f220969
zhoutianzi666 <17801055074@163.com>
2023-07-02 02:45:21 +0000

    add cublassex

--- cuda/sgemm/cublas.cu
+++ cuda/sgemm/cublas.cu
@@ -15,4 +15,20 @@ void cublas_matmul(cublasHandle_t& handle, DATATYPE *dev_a, DATATYPE *dev_b, DAT
 const float alpha = 1.0f;
 const float beta = 0.0f;
 cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, dev_b, n, dev_a, k, &beta, dev_c, n);
+
+cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, 
+                        m, k, 
+                        &alpha,  dev_b, 
+                            CUDA_R_32F, 
+                            n, 
+                            dev_a, 
+                            CUDA_R_32F, 
+                            k, 
+                            &beta, 
+                            dev_c, 
+                            CUDA_R_32F, 
+                            n, 
+                            CUDA_R_32F, 
+                            CUBLAS_GEMM_DEFAULT_TENSOR_OP);
+
 }
