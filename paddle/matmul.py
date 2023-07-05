57e7ebabed70faa7ff4640e3929644fef1f1f9bc
zhoutianzi666 <17801055074@163.com>
2023-07-05 14:38:13 +0800

    add paddle benchmark

--- paddle/matmul.py
+++ paddle/matmul.py
@@ -1,13 +1,14 @@
 # required: gpu
 import paddle
-
+paddle.set_device("gpu")
 m = 512
 n = 512
 k = 512
-A = paddle.rand((m, k))
-B = paddle.rand((k, n))
 WARMUP =  10
 REPEATE =  1000
+
+A = paddle.rand((m, k))
+B = paddle.rand((k, n))
 for i in range(WARMUP):
     A_out = paddle.matmul(A, B)
 
@@ -18,12 +19,12 @@ starttime = datetime.datetime.now()
 
 for i in range(REPEATE):
     A_out = paddle.matmul(A, B)
+paddle.device.cuda.synchronize(paddle.CUDAPlace(0))
 
 endtime = datetime.datetime.now()
 duringtime = endtime - starttime
 ms = duringtime.seconds * 1000 + duringtime.microseconds / 1000.0
 print (ms)# 单位是毫秒
-paddle.device.cuda.synchronize(paddle.CUDAPlace(0))
 Gflops = REPEATE * (m * n * k * 2 / 1000000) / ms
 print("Gflops: ", Gflops);
 
