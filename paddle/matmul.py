c85ee45947a4a63691a6c6b27399ba70b633e0ba
zhoutianzi666 <17801055074@163.com>
2023-07-05 15:00:59 +0800

    update mnatmul.py

--- paddle/matmul.py
+++ paddle/matmul.py
@@ -1,31 +1,44 @@
 # required: gpu
 import paddle
 paddle.set_device("gpu")
-m = 512
-n = 512
-k = 512
-WARMUP =  10
-REPEATE =  1000
 
-A = paddle.rand((m, k))
-B = paddle.rand((k, n))
-for i in range(WARMUP):
-    A_out = paddle.matmul(A, B)
 
-paddle.device.cuda.synchronize(paddle.CUDAPlace(0))
-import datetime
-import time
-starttime = datetime.datetime.now()
 
-for i in range(REPEATE):
-    A_out = paddle.matmul(A, B)
-paddle.device.cuda.synchronize(paddle.CUDAPlace(0))
+def gemm_flops(shape):
+    m=0
+    n=0
+    k=0
+    if(type(shape) == list):
+        m,n,k = shape[0],shape[1],shape[2]
+    else:
+        m=shape
+        n=shape
+        k=shape
 
-endtime = datetime.datetime.now()
-duringtime = endtime - starttime
-ms = duringtime.seconds * 1000 + duringtime.microseconds / 1000.0
-print (ms)# 单位是毫秒
-Gflops = REPEATE * (m * n * k * 2 / 1000000) / ms
-print("Gflops: ", Gflops);
+    WARMUP =  10
+    REPEATE =  1000
+    A = paddle.rand((m, k))
+    B = paddle.rand((k, n))
+    for i in range(WARMUP):
+        A_out = paddle.matmul(A, B)
 
+    paddle.device.cuda.synchronize(paddle.CUDAPlace(0))
+    import datetime
+    import time
+    starttime = datetime.datetime.now()
+
+    for i in range(REPEATE):
+        A_out = paddle.matmul(A, B)
+    paddle.device.cuda.synchronize(paddle.CUDAPlace(0))
+
+    endtime = datetime.datetime.now()
+    duringtime = endtime - starttime
+    ms = duringtime.seconds * 1000 + duringtime.microseconds / 1000.0
+    #print (ms)# 单位是毫秒
+    Tflops = REPEATE * (m * n * k * 2 / 1000000000) / ms
+    return Tflops
+    #print("Gflops: ", Gflops)
+
+for n in [128, 256, 512, 1024, 2048, 4096]:
+    print(gemm_flops(n))
 
