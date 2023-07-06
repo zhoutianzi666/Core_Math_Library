80d26c82b7e76d91251cf11585a20a2e6529b26f
zhoutianzi666 <17801055074@163.com>
2023-07-06 18:07:25 +0800

    commit

--- cuda/sgemm/cublas.cu
+++ cuda/sgemm/cublas.cu
@@ -15,13 +15,31 @@ void cublas_matmul(cublasHandle_t& handle, DATATYPE *dev_a, DATATYPE *dev_b, DAT
 const float alpha = 1.0f;
 const float beta = 0.0f;
 //cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, dev_b, n, dev_a, k, &beta, dev_c, n);
+cublasSetMathMode(handle, CUBLAS_TF32_TENSOR_OP_MATH);
+// cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, 
+//              n, m, k, &alpha,  
+//              dev_b, CUDA_R_32F, n, 
+//              dev_a, CUDA_R_32F, k, 
+//              &beta, 
+//             dev_c, CUDA_R_32F, n, 
+//             //CUBLAS_COMPUTE_32F_FAST_16F,
+//             CUBLAS_COMPUTE_32F_FAST_TF32,
+//             CUBLAS_GEMM_DFALT_TENSOR_OP);
 
-cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, 
-             n, m, k, &alpha,  
-             dev_b, CUDA_R_32F, n, 
-             dev_a, CUDA_R_32F, k, 
-             &beta, 
-            dev_c, CUDA_R_32F, n, 
-            CUBLAS_COMPUTE_32F_FAST_16F, 
-            CUBLAS_GEMM_DFALT_TENSOR_OP);
+// cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N,
+//                n, m, k, &alpha,
+//                dev_b, CUDA_R_32F, n,
+//                dev_a, CUDA_R_32F, k,
+//                &beta,
+//               dev_c, CUDA_R_32F, n,
+//               CUBLAS_COMPUTE_32F_FAST_TF32,
+//              CUBLAS_GEMM_DFALT_TENSOR_OP);
+
+cublasSgemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N,
+               n, m, k, &alpha,
+               dev_b, CUDA_R_32F, n,
+               dev_a, CUDA_R_32F, k,
+               &beta,
+              dev_c, CUDA_R_32F, n);
+
 }
