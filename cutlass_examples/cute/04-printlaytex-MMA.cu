c76def836976e551b25850225f54f7eb53119d33
zhoutianzi666 <17801055074@163.com>
2024-09-11 06:13:20 +0000

    add /zhoukangkang/2023-04-26SM80/Core_Math_Library/cutlass_examples/cute/04-printlaytex-MMA.cu

---
+++ cutlass_examples/cute/04-printlaytex-MMA.cu
@@ -0,0 +1,38 @@
+#include <cuda.h>
+#include <stdlib.h>
+
+#include <cute/tensor.hpp>
+
+#define PRINT(name, content) \
+    print(name);             \
+    print(" : ");            \
+    print(content);          \
+    print("\n");
+
+#define PRINTTENSOR(name, content) \
+    print(name);                   \
+    print(" : ");                  \
+    print_tensor(content);         \
+    print("\n");
+
+using namespace cute;
+int main() {
+    // mma
+    using mma_op = SM80_16x8x16_F32F16F16F32_TN;
+    using mma_traits = MMA_Traits<mma_op>;
+    using mma_atom = MMA_Atom<mma_traits>;
+    static constexpr int kMmaEURepeatM = 1;
+    static constexpr int kMmaEURepeatN = 1;
+    static constexpr int kMmaEURepeatK = 1;
+
+    using mma_atom_shape = mma_traits::Shape_MNK;
+    static constexpr int kMmaPM = 1;
+    static constexpr int kMmaPN = 1;
+    static constexpr int kMmaPK = 2;
+    using MMA_EU_RepeatT = decltype(make_layout(make_shape(
+        Int<kMmaEURepeatM>{}, Int<kMmaEURepeatN>{}, Int<kMmaEURepeatK>{})));
+    using MMA_P_T = Tile<Int<kMmaPM>, Int<kMmaPN>, Int<kMmaPK>>;
+    using MMA = decltype(make_tiled_mma(mma_atom{}, MMA_EU_RepeatT{}, MMA_P_T{}));
+    
+    print_latex(MMA{});
+}
