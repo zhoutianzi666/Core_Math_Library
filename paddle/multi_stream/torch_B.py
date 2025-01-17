c5c79e4499636e9fca9a937f64f8068af93e1489
chang-wenbin <changwenbin@baidu.com>
2025-01-17 11:46:55 +0800

    commit

---
+++ paddle/multi_stream/torch_B.py
@@ -0,0 +1,42 @@
+import torch
+
+
+
+def haha(a,b):
+    for i in range(10):
+        c = torch.mm(a,b)
+        a1 = c * 0.02 - 0.01
+        b1 = c * 0.01
+        a = torch.clip(a1, -1,1)
+        b = torch.clip(b1, -1,1)
+    return c
+
+typ = torch.bfloat16
+n = 5120
+a = torch.randn(n,n).type(typ).cuda()
+b = torch.randn(n,n).type(typ).cuda()
+
+
+
+new = torch.cuda.Stream()
+
+old = torch.cuda.current_stream()
+new.wait_stream(old)
+
+
+out0 = haha(a,b)
+
+torch.cuda.set_stream(new)
+
+print("sdcdsv")
+
+
+out1 = haha(a,b)
+
+old.wait_stream(new)
+torch.cuda.set_stream(old)
+
+torch.cuda.synchronize()
+print(out0-out1)
+
+
